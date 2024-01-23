bdd6a12a5822baaafa16ddf2354a4321ecc85ff6
KissMartin <kiss.martin2@students.jedlik.eu>
2024-01-23 11:03:31 +0100

    megoldas 4. feladatig

---
+++ eurovizio_megoldas.sql
@@ -0,0 +1,24 @@
+﻿# 2. feladat
+
+SELECT dalok.ev, dalok.orszag, dalok.eredeti
+FROM dalok
+INNER JOIN versenyek
+  ON dalok.orszag = versenyek.orszag
+    AND dalok.ev = versenyek.id
+ORDER BY dalok.ev;
+
+# 3. feladat
+
+SELECT dalok.sorrend
+FROM dalok
+WHERE dalok.helyezes = 1
+GROUP BY dalok.sorrend
+HAVING COUNT(dalok.id) >= 5
+
+# 4. feladat
+
+SELECT ev
+FROM dalok
+WHERE orszag IN ('Belgium', 'Hollandia', 'Luxemburg')
+GROUP BY ev
+HAVING COUNT(DISTINCT orszag) = 3;
