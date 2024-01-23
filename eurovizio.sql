8c23e9013df72c743de582ea0983a61bfc38431c
Kerpics-Katrin-Rebeka <98750568+Kerpics-Katrin-Rebeka@users.noreply.github.com>
2024-01-23 09:58:35 +0100

    Update eurovizio.sql

--- eurovizio.sql
+++ eurovizio.sql
@@ -7,34 +7,34 @@ COLLATE utf8_hungarian_ci;
 USE eurovizio;
 
 CREATE TABLE dalok (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    ev INT(11) NOT NULL,
-    sorrend INT(11) NOT NULL,
-    orszag VARCHAR(255) NOT NULL,
-    nyelv VARCHAR(255) NOT NULL,
-    eloado VARCHAR(255) NOT NULL,
-    eredeti VARCHAR(255) NOT NULL,
-    magyar VARCHAR(255) NOT NULL,
-    helyezes INT(11) NOT NULL,
-    pontszam INT(11) NOT NULL,
-	PRIMARY KEY (ID)
+    id int AUTO_INCREMENT UNIQUE,
+    ev int,
+    sorrend int,
+    orszag varchar(255),
+    nyelv varchar(255),
+    eloado varchar(255),
+    eredeti varchar(255),
+    magyar varchar(255),
+    helyezes int,
+    pontszam int,
+    PRIMARY KEY (id)
 );
 LOAD DATA INFILE 'dal.txt' INTO TABLE dalok CHARACTER SET UTF8 FIELDS TERMINATED BY '\t' IGNORE 1 LINES;
 
 CREATE TABLE versenyek (
-  id int(11) NOT NULL,
-  datum date NOT NULL,
-  varos varchar(255) NOT NULL,
-  orszag varchar(255) NOT NULL,
-  induloszam tinyint(1) NOT NULL,
+  id int NOT NULL UNIQUE,
+  datum date,
+  varos varchar(255),
+  orszag varchar(255),
+  induloszam tinyint(1),
   PRIMARY KEY (id)
 );
 LOAD DATA INFILE 'verseny.txt' INTO TABLE versenyek CHARACTER SET UTF8 FIELDS TERMINATED BY '\t' IGNORE 1 LINES;
 
 CREATE TABLE nyelvek (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	orszag varchar(255) NOT NULL,
-	nyelv varchar(255) NOT NULL,	
+	id int AUTO_INCREMENT UNIQUE,
+	orszag varchar(255),
+	nyelv varchar(255),	
     PRIMARY KEY (id)
 );
 LOAD DATA INFILE 'nyelv.txt' INTO TABLE nyelvek CHARACTER SET UTF8 FIELDS TERMINATED BY '\t' IGNORE 1 LINES;
