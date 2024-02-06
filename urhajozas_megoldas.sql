9a6a6125797bd91b3e888b5df342134cc0808258
Kerpics-Katrin-Rebeka <kerpics.katrin.rebeka@students.jedlik.eu>
2024-02-06 11:13:56 +0100

    idontcare

---
+++ urhajozas_megoldas.sql
@@ -0,0 +1,33 @@
+﻿-- 2. feladat:
+SELECT nev, urido
+FROM urhajosok
+WHERE nem = 'N'
+ORDER BY urido DESC
+LIMIT 1;
+
+-- 3. feladat:
+SELECT megnevezes, DATEDIFF(veg, kezdet) AS hossz
+FROM kuldetesek
+WHERE YEAR(kezdet) != YEAR(veg);
+
+-- 4. feladat: 
+--
+
+-- 5. feladat:
+SELECT orszag, COUNT(id) AS urhajosok_szama
+FROM urhajosok
+GROUP BY orszag
+ORDER BY urhajosok_szama DESC;
+
+-- 6. feladat:
+SELECT COUNT(DISTINCT orszag) AS orszagszam
+FROM urhajosok;
+
+-- 7. feladat:
+--
+
+-- 8. feladat:
+SELECT k.megnevezes, k.kezdet
+FROM kuldetesek k
+WHERE YEAR(k.kezdet) BETWEEN 1991 AND 2000
+ORDER BY k.kezdet, k.megnevezes;
