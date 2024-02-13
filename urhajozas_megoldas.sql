1573989da1640c33de798ae779b3622db2674b9a
KissMartin <kiss.martin2@students.jedlik.eu>
2024-02-13 10:39:23 +0100

    4, 7. feldatok

--- urhajozas_megoldas.sql
+++ urhajozas_megoldas.sql
@@ -11,7 +11,20 @@ FROM kuldetesek
 WHERE YEAR(kezdet) != YEAR(veg);
 
 -- 4. feladat: 
---
+SELECT 
+    urhajosok.nev AS urhajos_neve,
+    MIN(YEAR(kuldetesek.kezdet)) - urhajosok.szulev AS kezdet_kor,
+    MAX(YEAR(kuldetesek.veg)) - urhajosok.szulev AS veg_kor
+FROM 
+    urhajosok
+JOIN 
+    repulesek ON urhajosok.id = repulesek.urhajosId
+JOIN 
+    kuldetesek ON repulesek.kuldetesId = kuldetesek.id
+GROUP BY urhajosok.id
+HAVING COUNT(repulesek.kuldetesId) > 1
+ORDER BY urhajosok.nev;
+
 
 -- 5. feladat:
 SELECT orszag, COUNT(id) AS urhajosok_szama
@@ -24,7 +37,18 @@ SELECT COUNT(DISTINCT orszag) AS orszagszam
 FROM urhajosok;
 
 -- 7. feladat:
---
+SELECT DISTINCT k.megnevezes AS kuldetes_neve
+FROM kuldetesek k
+JOIN repulesek r ON k.id = r.kuldetesId
+JOIN urhajosok u ON r.urhajosId = u.id
+WHERE u.nem = 'F' AND EXISTS (
+    SELECT 1
+    FROM repulesek r2
+    JOIN urhajosok u2 ON r2.urhajosId = u2.id
+    WHERE r2.kuldetesId = k.id AND u2.nem = 'N'
+)
+ORDER BY kuldetes_neve ASC;
+
 
 -- 8. feladat:
 SELECT k.megnevezes, k.kezdet
